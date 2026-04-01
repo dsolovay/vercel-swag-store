4b05d1eceb7aecee60c50f81ec4334ab92e67603
dsolovay <dan.solovay@velir.com>
2026-03-31 22:21:01 -0400

    Add metadata

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,8 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Search",
+  description: "Search for your favorite Vercel swag items.",
+  openGraph: {
+    title: "Search",
+    description: "Search for your favorite Vercel swag items.",
+  },
+};
 export default function Search() {
-    return (
-        <div>
-            <h1>Search Page</h1>
-            <p>This is the search page.</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Search Page</h1>
+      <p>This is the search page.</p>
+    </div>
+  );
 }
