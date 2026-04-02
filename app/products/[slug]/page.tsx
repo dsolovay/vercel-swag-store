7544d4e2c56fc335dd4fefeff9be59f2ec96279f
dsolovay <dan.solovay@velir.com>
2026-04-02 10:05:16 -0400

    pdp and notes

---
+++ app/products/[slug]/page.tsx
@@ -0,0 +1,15 @@
+export default function ProductDetailPage() {
+  return (
+    <div>
+      <h1>Product Detail Page</h1>
+      <p>This is the product detail page.</p>
+    </div>
+  );
+}
+
+// TODOS
+// get params
+// get static params (confirm dynamically built)
+// Add api route for product details
+// Confirm all requirements set
+// Evaluation caching ttl settings
